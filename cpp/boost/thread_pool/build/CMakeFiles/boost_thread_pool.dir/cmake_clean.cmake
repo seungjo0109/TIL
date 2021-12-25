bb0cfb98b1e73a0446a19ee1b48b35281b430599
seungjo0109 <seungjo0109@gmail.com>
2021-12-25 17:56:35 +0900

    Init boost_thread_pool workspace

---
+++ cpp/boost/thread_pool/build/CMakeFiles/boost_thread_pool.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boost_thread_pool.dir/src/main.cpp.o"
+  "boost_thread_pool.pdb"
+  "boost_thread_pool"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boost_thread_pool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/boost/thread_pool/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, char *argv[]){
+
+    std::cout<< "Init thread pool workspace" << std::endl;
+
+    return 0;
+}
